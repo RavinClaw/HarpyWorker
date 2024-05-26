c9b56e36279c5ff83406343dfb95bd41c868f6dc
Ravin <87692753+RavinClaw@users.noreply.github.com>
2024-05-26 05:50:19 +0100

    The Main Turtle File
    This is the current version nothing is concrete yet

---
+++ main.lua
@@ -0,0 +1,70 @@
+local ws = http.websocket("ws://192.168.1.25:5858")
+
+local json_message = {
+    ["username"] = os.getComputerLabel(),
+    ["id"] = os.getComputerID()
+}
+
+local raw_message = textutils.serialiseJSON(json_message)
+ws.send(raw_message)
+
+
+
+while true do
+    local raw_info = ws.receive()
+    local json_info = textutils.unserialiseJSON(raw_info)
+
+    print(json_info)
+    local response = {
+        ["success"] = false
+    }
+
+    if json_info == nil then
+        -- This literraly does nothing
+
+
+    elseif json_info["action"] == "movement" then
+        if json_info["type"] == "UP" then
+            turtle.up()
+            response["success"] = true
+
+        elseif json_info["type"] == "DOWN" then
+            turtle.down()
+            response["success"] = true
+
+        elseif json_info["type"] == "FORWARD" then
+            turtle.forward()
+            response["success"] = true
+
+        elseif json_info["type"] == "BACKWARD" then
+            turtle.back()
+            response["success"] = true
+
+        elseif json_info["type"] == "RIGHT" then
+            turtle.turnRight()
+            response["success"] = true
+
+        elseif json_info["type"] == "LEFT" then
+            turtle.turnLeft()
+            response["success"] = true
+        end
+
+
+    elseif json_info["action"] == "inspect" then
+        response["success"] = true
+        response["block"] = turtle.inspect()
+    
+
+    elseif json_info["action"] == "fuel-level" then
+        response["success"] = true
+        response["fuel-level"] = turtle.getFuelLevel()
+
+    
+    elseif json_info["action"] == "selected-slot" then
+        response["success"] = true
+        response["slot"] = turtle.getSelectedSlot()
+    end
+
+    -- Sends a response to the client
+    ws.send(textutils.serialiseJSON(response))
+end
